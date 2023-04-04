bae68d2c7acdef4abfb12dd1666778be928ff445
Benson <njugunab655@gmail.com>
2023-04-04 20:26:31 +0300

    Add solution and tests for reverseString

---
+++ reverseString.test.js
@@ -0,0 +1,11 @@
+const reverseString = require('./reverseString');
+
+test('Not a valid string', () => {
+  expect(() => {
+    reverseString('')
+  }).toThrow('Enter a valid string!');
+})
+
+test('Perfect reverse', () => {
+  expect(reverseString('book')).toBe('koob');
+})
