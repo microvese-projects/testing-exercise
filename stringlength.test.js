b09bb16e430ce6d0acb2c6eb5ccea01d111a89fc
Benson <njugunab655@gmail.com>
2023-04-04 20:25:51 +0300

    Add solution and tests for stringlength

---
+++ stringlength.test.js
@@ -0,0 +1,11 @@
+const stringLength = require('./stringLength');
+
+test('The length of string should be:', () => {
+  expect(stringLength('ben')).toBe(3);
+})
+
+test('Scope of string', () => {
+  expect(() => {
+    stringLength('');
+  }).toThrow('String is out of scope');
+})
